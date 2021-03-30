7896f9ca9c70cd82a569156403706ec3d0a4cfcb
Manny Morales <manny@movingartwork.net>
2021-03-30 18:21:20 -0400

    Changed to green

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-var CVS = document.querySelector("canvas");
-var C = CVS.getContext("2d");
-C.fillStyle = "green";
-C.fillRect( 0, 0, 800, 800 );
+//globals
+function init(){
+	// init globals
+}
+function main(){
+	init();
+	var CVS = document.querySelector("canvas");
+	var C = CVS.getContext("2d");
+	C.fillStyle = "red";
+	C.fillRect( 0, 0, 800, 800 );
+}
+main();
