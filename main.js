9611d536f67c84f38ded0ed33716f6b0f9e63a1e
Manny Morales <manny@movingartwork.net>
2021-03-30 18:23:38 -0400

    Black

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function main(){
 	init();
 	var CVS = document.querySelector("canvas");
 	var C = CVS.getContext("2d");
-	C.fillStyle = "red";
+	C.fillStyle = "black";
 	C.fillRect( 0, 0, 800, 800 );
 }
 main();
